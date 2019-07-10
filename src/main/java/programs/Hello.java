5417ee44a68b194073beb23a9e73322ff1db50e9
Avinash Seelam <avinash120196@gmail.com>
2019-07-10 22:40:27 +0530

    Simple maven project is commited

---
+++ src/main/java/programs/Hello.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package programs;
+
+/**
+ * @author Avinash
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Avinash");
+	}
+
+}

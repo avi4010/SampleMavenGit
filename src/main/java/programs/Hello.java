acf1c05690263b41d0ec5b85a19a58f30e8113ff
avi4010 <avinash120196@gmail.com>
2019-07-24 13:56:02 +0000

    Jenkins trigger trail 4

--- src/main/java/programs/Hello.java
+++ src/main/java/programs/Hello.java
@@ -15,7 +15,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Avinash");
 		System.out.println("This is my second commit");
-		System.out.println("This changes trigers a build in Jenkins 3");
+		System.out.println("This changes trigers a build in Jenkins 4");
 	}
 
 }
